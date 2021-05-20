71d11e804964986133847f407a10f9298d81a052
Victor Hugo Carreon Pulido <hv_carreon@hotmail.com>
2021-05-20 17:59:07 -0500

    Se agrego un control que es un panel redondo.

---
+++ CustomControls/Rounded Panel.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace CustomControls
+{
+    partial class Rounded_Panel
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ CustomControls/Rounded Panel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomControls
+{
+    public partial class Rounded_Panel : Panel
+    {
+        public Color PanelColor { get; set; }
+        public Int32 Radio { get; set; }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.FillRoundedRectangle(new SolidBrush(PanelColor), 0, 0, this.Width, this.Height, Radio);
+            SolidBrush brush = new SolidBrush(PanelColor);
+        }
+
+        public Rounded_Panel()
+        {
+            InitializeComponent();
+        }
+
+        public Rounded_Panel(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
