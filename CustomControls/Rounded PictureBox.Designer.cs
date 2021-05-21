69daf551eeb6b7744c89e9efdca89267f1b7b8cb
Victor Hugo Carreon Pulido <hv_carreon@hotmail.com>
2021-05-20 19:41:49 -0500

    Elementos necesarios para un picturebox redondeado.

---
+++ CustomControls/Rounded PictureBox.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace CustomControls
+{
+    partial class Rounded_PictureBox
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ CustomControls/Rounded PictureBox.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomControls
+{
+    public partial class Rounded_PictureBox : PictureBox
+    {
+        public Color ColorMargin { get; set; }
+        public Int32 WidhtMargin { get; set; }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            GraphicsPath g = new GraphicsPath();
+            Graphics gr = pe.Graphics;
+            gr.DrawEllipse(new Pen(ColorMargin, WidhtMargin), 0, 0, this.Width, this.Height);
+            g.AddEllipse(0,0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(g);
+            base.OnPaint(pe);
+        }
+
+        public Rounded_PictureBox()
+        {
+            InitializeComponent();
+        }
+
+        public Rounded_PictureBox(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
