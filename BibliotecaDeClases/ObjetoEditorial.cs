0c91a85e0901b6a7588f77bfa9509dd2b9f2260d
Victor Hugo Carreon Pulido <hv_carreon@hotmail.com>
2021-05-26 23:21:27 -0500

    .

---
+++ BibliotecaDeClases/ObjetoEditorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDeClases
+{
+    public class ObjetoEditorial
+    {
+        public ObjetoEditorial()
+        {
+
+        }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+    }
+}
